5b82921c6b58af9544e2979bf605339e71bee1b6
ahmdirfnz <irfanzorro6985@gmail.com>
2022-07-17 19:08:21 +0800

    implement register

--- lib/registration.dart
+++ lib/registration.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class MyRegister extends StatefulWidget {
   const MyRegister({Key? key}) : super(key: key);
@@ -8,137 +11,218 @@ class MyRegister extends StatefulWidget {
 }
 
 class _MyRegisterState extends State<MyRegister> {
+  // final _fireStore = FirebaseFirestore.instance;
+  // final _auth = FirebaseAuth.instance;
+  // late User loggedInUser;
+
+  final TextEditingController _fullNameController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _matricNumberController = TextEditingController();
+  final TextEditingController _mobileNumberController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  
+  final CollectionReference _students = FirebaseFirestore.instance.collection('student');
+
+  Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
+    if (documentSnapshot != null) {
+      _fullNameController.text = documentSnapshot['name'];
+      _usernameController.text = documentSnapshot['username'];
+      _matricNumberController.text = documentSnapshot['matricNumber'];
+      _mobileNumberController.text = documentSnapshot['mobileNumber'];
+      _emailController.text = documentSnapshot['email'];
+      _passwordController.text = documentSnapshot['password'];
+
+      final String? fullName = _fullNameController.text;
+      final String? username = _usernameController.text;
+      final String? matricNumber = _matricNumberController.text;
+      final int? mobileNumber = _mobileNumberController.text as int?;
+      final String? email = _emailController.text;
+      final String? password = _passwordController.text;
+
+      await _students.add({
+        "fullName": fullName,
+        "username": username,
+        "matricNumber": matricNumber,
+        "mobileNumber": mobileNumber,
+        "email": email,
+        "password": password,
+
+
+      });
+
+      _fullNameController.text = '';
+      _usernameController.text = '';
+      _matricNumberController.text = '';
+      _mobileNumberController.text = '';
+      _emailController.text = '';
+      _passwordController.text = '';
+
+
+
+    }
+  }
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Registration Page'),
-      ),
       backgroundColor: Colors.white,
       body: Form(
-        child: ListView(
-          padding: EdgeInsets.all(10.0),
-          children: <Widget>[
-            const TextField(
-              decoration: InputDecoration(
-                  labelText: 'Full Name',
-                  prefixIcon: Icon(Icons.person),
-                  suffixIcon: Icon(
-                    Icons.delete_outline,
-                    color: Colors.red,
+        child: Center(
+          child: ListView(
+            padding: const EdgeInsets.all(10.0),
+            shrinkWrap: true,
+            children: <Widget>[
+              const Center(
+                  child: Text(
+                'Registration Page',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+              )),
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _fullNameController,
+                  decoration: const InputDecoration(
+                      labelText: 'Full Name',
+                      prefixIcon: Icon(Icons.person),
+                      suffixIcon: Icon(
+                        Icons.delete_outline,
+                        color: Colors.red,
+                      ),
+                      hintText: "Enter Your Full Name",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide: BorderSide(color: Colors.black, width: 2.0),
+                      )),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _usernameController,
+                  decoration: const InputDecoration(
+                      labelText: 'Username',
+                      prefixIcon: Icon(Icons.person),
+                      suffixIcon: Icon(
+                        Icons.delete_outline,
+                        color: Colors.red,
+                      ),
+                      hintText: "Enter Username",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                        borderSide: BorderSide(color: Colors.black, width: 2.0),
+                      )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _matricNumberController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Matric Number',
+                    hintText: 'Enter Your Matric Number',
+                    prefixIcon: Icon(Icons.school),
+                    suffixIcon: Icon(
+                      Icons.delete_outline,
+                      color: Colors.red,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                    ),
                   ),
-                  hintText: "Enter Your Full Name",
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                    borderSide: BorderSide(color: Colors.black, width: 2.0),
-                  )),
-            ),
-            SizedBox(height: 10.0,),
-            const TextField(
-              decoration: InputDecoration(
-                  labelText: 'Username',
-                  prefixIcon: Icon(Icons.person),
-                  suffixIcon: Icon(
-                    Icons.delete_outline,
-                    color: Colors.red,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _mobileNumberController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    labelText: 'Mobile Number',
+                    hintText: 'Enter Mobile Number',
+                    prefixIcon: Icon(Icons.phone),
+                    suffixIcon: Icon(
+                      Icons.delete_outline,
+                      color: Colors.red,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                    ),
                   ),
-                  hintText: "Enter Username",
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                    borderSide: BorderSide(color: Colors.black, width: 2.0),
-                  )),
-            ),
-            SizedBox(height: 10.0,),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Matric Number',
-                hintText: 'Enter Your Matric Number',
-                prefixIcon: Icon(Icons.school),
-                suffixIcon: Icon(
-                  Icons.delete_outline,
-                  color: Colors.red,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(color: Colors.black, width: 2.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
                 ),
               ),
-            ),
-            SizedBox(height: 10.0,),
-            TextFormField(
-              keyboardType: TextInputType.number,
-              decoration: const InputDecoration(
-                labelText: 'Mobile Number',
-                hintText: 'Enter Mobile Number',
-                prefixIcon: Icon(Icons.phone),
-                suffixIcon: Icon(
-                  Icons.delete_outline,
-                  color: Colors.red,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(color: Colors.black, width: 2.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
-                ),
-              ),
-            ),
-            SizedBox(height: 10.0,),
-            TextFormField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                hintText: 'Enter Your Email',
-                prefixIcon: Icon(Icons.email),
-                suffixIcon: Icon(
-                  Icons.delete_outline,
-                  color: Colors.red,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(color: Colors.black, width: 2.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter Your Email',
+                    prefixIcon: Icon(Icons.email),
+                    suffixIcon: Icon(
+                      Icons.delete_outline,
+                      color: Colors.red,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                    ),
+                  ),
                 ),
               ),
-            ),
-
-            TextFormField(
-              keyboardType: TextInputType.visiblePassword,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                hintText: 'Enter Your Password',
-                prefixIcon: Icon(Icons.password),
-                suffixIcon: Icon(
-                  Icons.delete_outline,
-                  color: Colors.red,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(color: Colors.black, width: 2.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                  borderSide: BorderSide(color: Colors.blue, width: 2.0),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter Your Password',
+                    prefixIcon: Icon(Icons.password),
+                    suffixIcon: Icon(
+                      Icons.delete_outline,
+                      color: Colors.red,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                      borderSide: BorderSide(color: Colors.blue, width: 2.0),
+                    ),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-          Navigator.pushNamed(context, 'room_screen');
-        }),
+        onPressed: () => _create(),
         tooltip: 'Next Page',
         child: const Icon(Icons.navigate_next),
       ),
@@ -238,100 +322,29 @@ class _InfoPageState extends State<InfoPage> {
       appBar: AppBar(
         title: Text('Info Page'),
       ),
-      body:  Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: const [
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Username',
-                    prefixIcon: Icon(Icons.person),
-                    suffixIcon: Icon(
-                      Icons.delete_outline,
-                      color: Colors.red,
-                    ),
-                    hintText: "Enter Username",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      borderSide: BorderSide(color: Colors.black, width: 2.0),
-                    )),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Username',
-                    prefixIcon: Icon(Icons.person),
-                    suffixIcon: Icon(
-                      Icons.delete_outline,
-                      color: Colors.red,
-                    ),
-                    hintText: "Enter Username",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      borderSide: BorderSide(color: Colors.black, width: 2.0),
-                    )),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Username',
-                    prefixIcon: Icon(Icons.person),
-                    suffixIcon: Icon(
-                      Icons.delete_outline,
-                      color: Colors.red,
-                    ),
-                    hintText: "Enter Username",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      borderSide: BorderSide(color: Colors.black, width: 2.0),
-                    )),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Username',
-                    prefixIcon: Icon(Icons.person),
-                    suffixIcon: Icon(
-                      Icons.delete_outline,
-                      color: Colors.red,
-                    ),
-                    hintText: "Enter Username",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      borderSide: BorderSide(color: Colors.black, width: 2.0),
-                    )),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Username',
-                    prefixIcon: Icon(Icons.person),
-                    suffixIcon: Icon(
-                      Icons.delete_outline,
-                      color: Colors.red,
-                    ),
-                    hintText: "Enter Username",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      borderSide: BorderSide(color: Colors.black, width: 2.0),
-                    )),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                    labelText: 'Username',
-                    prefixIcon: Icon(Icons.person),
-                    suffixIcon: Icon(
-                      Icons.delete_outline,
-                      color: Colors.red,
-                    ),
-                    hintText: "Enter Username",
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0)),
-                      borderSide: BorderSide(color: Colors.black, width: 2.0),
-                    )),
-              ),
-            ],
+      body: Center(
+          child: ListView(
+        children: const [
+          Card(
+              child: ListTile(
+            title: Text("List Item 1"),
           )),
+          Card(
+            child: ListTile(
+              title: Text("List Item 2"),
+            ),
+          ),
+          Card(
+              child: ListTile(
+            title: Text("List Item 3"),
+          )),
+        ],
+        padding: EdgeInsets.all(10),
+      )),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => setState(() {
-          Navigator.pushNamed(context, 'info_screen');
-        }),
+        onPressed: () {
+          FirebaseFirestore.instance.collection('student').add({'text': 'data added'});
+        },
         tooltip: 'Next Page',
         child: const Icon(Icons.navigate_next),
       ),
